bd8c2bd874c7da033f0391b941e2fd38253d5e9a
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2018-02-26 21:49:38 -0300

    Send email with logged work time

--- app/src/main/java/br/com/optimizer7/logworktime/ListLoggedWorktime.kt
+++ app/src/main/java/br/com/optimizer7/logworktime/ListLoggedWorktime.kt
@@ -7,14 +7,17 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.text.Html
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import br.com.optimizer7.logworktime.Model.Worktime
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -36,12 +39,13 @@ class ListLoggedWorktime : AppCompatActivity() {
     private var monthSelected = 0
     private var yearSelected = 0
     private val mLogWorktimeRef = mRootRef.child("logworktimes")
-    private lateinit var currentUser: FirebaseAuth
+    private var currentUser: FirebaseUser? = null
     private var cal = Calendar.getInstance()
     private val monthSimpleDateFormat = SimpleDateFormat("MMMM", getCurrentLocale())
     private var txtSelectedMonth: TextView? = null
     private var previousMonth: ImageView? = null
     private var nextMonth: ImageView? = null
+    private var sendEmail: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,7 +71,7 @@ class ListLoggedWorktime : AppCompatActivity() {
         myToolbar.inflateMenu(R.menu.list_main_menu)
 
         // Check if user is signed in (non-null) and update UI accordingly.
-        currentUser = FirebaseAuth.getInstance()
+        currentUser = FirebaseAuth.getInstance().currentUser
 
         bindViews()
 
@@ -83,6 +87,7 @@ class ListLoggedWorktime : AppCompatActivity() {
         previousMonth = findViewById(R.id.previousMonth)
         nextMonth = findViewById(R.id.nextMonth)
         txtSelectedMonth = findViewById(R.id.txtSelectedMonth)
+        sendEmail = findViewById(R.id.btSendEmail)
 
     }
 
@@ -159,6 +164,50 @@ class ListLoggedWorktime : AppCompatActivity() {
             cal.add(Calendar.MONTH, 1)
             updateDateWorktimeToLog()
         })
+
+        sendEmail!!.setOnClickListener ({
+            val intent = Intent(Intent.ACTION_SEND)
+
+            intent.type = "text/html"
+
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(currentUser!!.email!!))
+
+            val appName = getString(R.string.app_name)
+
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.send_email_subject_label, getString(R.string.app_name), monthSelectedText))
+
+            intent.putExtra(Intent.EXTRA_TEXT, buildBodyEmailLoggedWorkTimeSheet())
+
+            startActivity(intent)
+        })
+    }
+
+    private fun buildBodyEmailLoggedWorkTimeSheet() : String{
+
+        val stringBuilder = StringBuilder()
+
+        stringBuilder.appendln("-----------------------------------")
+        stringBuilder.appendln(monthSelectedText)
+        stringBuilder.appendln("-----------------------------------")
+
+        for(loggedWorkTime in listOfWorktime){
+            stringBuilder.appendln("")
+            stringBuilder.appendln("\t| " + loggedWorkTime.date + "\t| ")
+            stringBuilder.append("\t| " + isEmptyOrNull(loggedWorkTime.beginWorktime) + "\t| ")
+            stringBuilder.append(isEmptyOrNull(loggedWorkTime.beginLunch) + "\t| ")
+            stringBuilder.append(isEmptyOrNull(loggedWorkTime.doneLunch) + "\t| ")
+            stringBuilder.append(isEmptyOrNull(loggedWorkTime.doneWorktime) + "\t|\n")
+        }
+
+        return stringBuilder.toString()
+    }
+
+    private fun isEmptyOrNull(data: String?) : String{
+        return if(data.equals("") || data == null){
+            "     N/A     "
+        } else{
+            data
+        }
     }
 
     private fun loadLoggedWorktime(){
@@ -176,8 +225,8 @@ class ListLoggedWorktime : AppCompatActivity() {
                 mAdapter!!.notifyDataSetChanged()
 
                 dataSnapshot
-                        ?.child(currentUser.uid)
-                        ?.child(currentUser.currentUser!!.displayName)
+                        ?.child(currentUser!!.uid)
+                        ?.child(currentUser!!.displayName)
                         ?.child(yearSelectedText)
                         ?.child(monthSelectedText)
                         ?.children
